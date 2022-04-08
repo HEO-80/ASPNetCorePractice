44623938fa5727a0be800b3035b33e55727042fe
Hector Oviedo <hectoroviedo@social11.es>
2022-04-08 12:46:40 +0200

    Ex1.5 Entity Framework core

--- MasterNet2/MasterNet/Models/Post.cs
+++ MasterNet2/MasterNet/Models/Post.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MasterNet.Models
 {
     public class Post
     {
-
+        //introduction over project
         public int Id { get; set; }
 
         [Required]
@@ -15,6 +16,14 @@ namespace MasterNet.Models
         public string Content { get; set; }
         [Range (0,5)]
         public int Score { get; set; }
+
+        //Foreing Key
+        public int? TagId { get; set; }
+
+        [ForeignKey(name: "TagId")]
+        public Tag Tag { get; set; }
+
+
 
     }
 }
